62b1297004372b9eec5aafdd63368e760d6e4fe2
piotrpawlowski7 <74098973+piotrpawlowski7@users.noreply.github.com>
2021-09-18 20:02:40 +0200

    first

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,6 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-design-assets/register",
     "@storybook/addon-notes/register",
+    '@whitespace/storybook-addon-html',
   ],
 };
--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,6 +1,7 @@
 import "./button.css";
 import buttonText from "./button-text.html";
 import buttonLogo from "./button-logo.html";
+import buttonHome from "./button-home.html";
 
 export default {
   title: "Components/Button",
@@ -9,3 +10,5 @@ export default {
 
 export const withText = () => buttonText;
 export const withLogo = () => buttonLogo;
+
+export const onHome = () => buttonHome;
